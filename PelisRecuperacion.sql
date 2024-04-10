c6ee51df343375559dd4b8163a6024b57bf72fed
Gabriel Alonso <galonsov22@fpcoruna.afundacion.org>
2024-04-10 09:58:55 +0200

    Añado estructura base de datos

---
+++ PelisRecuperacion.sql
@@ -0,0 +1,189 @@
+-- MariaDB dump 10.19  Distrib 10.11.4-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: PelisRecuperacion
+-- ------------------------------------------------------
+-- Server version	10.11.4-MariaDB-1~deb12u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Amigos`
+--
+
+DROP TABLE IF EXISTS `Amigos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Amigos` (
+  `id_usuario` int(11) NOT NULL,
+  `id_amigo` int(11) NOT NULL,
+  PRIMARY KEY (`id_usuario`,`id_amigo`),
+  KEY `id_amigo` (`id_amigo`),
+  CONSTRAINT `Amigos_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `Usuarios` (`id`),
+  CONSTRAINT `Amigos_ibfk_2` FOREIGN KEY (`id_amigo`) REFERENCES `Usuarios` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Amigos`
+--
+
+LOCK TABLES `Amigos` WRITE;
+/*!40000 ALTER TABLE `Amigos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Amigos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Favoritas`
+--
+
+DROP TABLE IF EXISTS `Favoritas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Favoritas` (
+  `id_usuario` int(11) NOT NULL,
+  `id_pelicula` int(11) NOT NULL,
+  PRIMARY KEY (`id_usuario`,`id_pelicula`),
+  KEY `id_pelicula` (`id_pelicula`),
+  CONSTRAINT `Favoritas_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `Usuarios` (`id`),
+  CONSTRAINT `Favoritas_ibfk_2` FOREIGN KEY (`id_pelicula`) REFERENCES `Peliculas` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Favoritas`
+--
+
+LOCK TABLES `Favoritas` WRITE;
+/*!40000 ALTER TABLE `Favoritas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Favoritas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Peliculas`
+--
+
+DROP TABLE IF EXISTS `Peliculas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Peliculas` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `imagen` varchar(255) DEFAULT NULL,
+  `categoria` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Peliculas`
+--
+
+LOCK TABLES `Peliculas` WRITE;
+/*!40000 ALTER TABLE `Peliculas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Peliculas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Reseñas`
+--
+
+DROP TABLE IF EXISTS `Reseñas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Reseñas` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `id_pelicula` int(11) DEFAULT NULL,
+  `comentario` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_usuario` (`id_usuario`),
+  KEY `id_pelicula` (`id_pelicula`),
+  CONSTRAINT `Reseñas_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `Usuarios` (`id`),
+  CONSTRAINT `Reseñas_ibfk_2` FOREIGN KEY (`id_pelicula`) REFERENCES `Peliculas` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Reseñas`
+--
+
+LOCK TABLES `Reseñas` WRITE;
+/*!40000 ALTER TABLE `Reseñas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Reseñas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Usuarios`
+--
+
+DROP TABLE IF EXISTS `Usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `apellidos` varchar(255) DEFAULT NULL,
+  `nickname` varchar(255) DEFAULT NULL,
+  `contraseña` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `fecha` datetime DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `token` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Usuarios`
+--
+
+LOCK TABLES `Usuarios` WRITE;
+/*!40000 ALTER TABLE `Usuarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Vistas`
+--
+
+DROP TABLE IF EXISTS `Vistas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Vistas` (
+  `id_usuario` int(11) NOT NULL,
+  `id_pelicula` int(11) NOT NULL,
+  PRIMARY KEY (`id_usuario`,`id_pelicula`),
+  KEY `id_pelicula` (`id_pelicula`),
+  CONSTRAINT `Vistas_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `Usuarios` (`id`),
+  CONSTRAINT `Vistas_ibfk_2` FOREIGN KEY (`id_pelicula`) REFERENCES `Peliculas` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Vistas`
+--
+
+LOCK TABLES `Vistas` WRITE;
+/*!40000 ALTER TABLE `Vistas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Vistas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-04-10  9:48:30
